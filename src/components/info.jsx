d4672f53a4b41b544625c02d93115adba3ab079f
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-03-26 17:38:28 +0530

    add edit button and include its functionality

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,38 +1,73 @@
+import {useState} from 'react';
 import { EditButton } from "./editButton";
 import { btnStyle } from "../styles/button";
 import { formStyle } from "../styles/button";
 
 function GeneralInfo(){
+  const [info, setInfo]= useState({name: "Gaurav Shukla", email: "240108029@hbtu.ac.in" , phone: 6394928581});
+  const [inputName, setInputName]= useState({name:"" , email:"", phone:""});
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setInfo({ ...info, name: inputName.name, email: inputName.email, phone: inputName.phone});
+  }
+
+
     return (
       <div>
         <h1>General Information</h1>;
         <h2>
-            Gaurav Shukla <br />
-            240108029@hbtu.ac.in <br />
-            6394928581
+            {info.name} <br />
+            {info.email}<br />
+            {info.phone}
         </h2>
 
         <EditButton buttonStyle={btnStyle}/>
 
-        <form style={formStyle}>
+        <form style={formStyle} onSubmit={handleSubmit}>
           <label >
             Name:-
-            <input style={formStyle} type= "text" name="name" id="name"/>
+            <input 
+                style={formStyle} 
+                type= "text" 
+                name="name"
+                id="name"
+                value={inputName.name}
+                onChange={(e) => setInputName({...inputName, name: e.target.value})}
+            />
           </label>
 
           <br />
 
           <label >
             Email:-
-            <input style={formStyle} type= "email" name="name" id="name"/>
+            <input 
+                style={formStyle}
+                type= "email"
+                name="name"
+                id="name"
+                value={inputName.email}
+                onChange={(e) => setInputName({...inputName, email: e.target.value})}
+            />
           </label>
 
           <br />
 
           <label >
             Phone:-
-            <input style={formStyle} type= "number" name="name" id="name"/>
+            <input 
+                style={formStyle} 
+                type= "number"
+                name="name" 
+                id="name"
+                value={inputName.phone}
+                onChange={(e) => setInputName({...inputName, phone: e.target.value})}
+            />
           </label>
+
+          <br />
+
+          <button>Submit</button>
         </form>
       </div>
     );
